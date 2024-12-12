c5fb6d2df3d6a7a39bf79c126ec2c95a89dcdee9
seon207 <83705020+seon207@users.noreply.github.com>
2024-12-12 18:54:15 +0900

    [Silver III] Title: N과 M (8), Time: 160 ms, Memory: 18880 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// BOJ_15656 N과 M (7)
+public class Main {
+    static int N, M;
+    static int[] numbers;
+    static ArrayList<Integer> list;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+
+        list = new ArrayList<>();
+        numbers = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);   // 정렬
+
+        comb(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void comb(int depth, int start) {
+        if (depth == M) {
+            for (int num : list) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            list.add(numbers[i]);
+            comb(depth + 1, i);
+            list.remove(list.size() - 1);
+        }
+    }
+}

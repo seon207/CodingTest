8edc8863bf5fce63abad2fd28184e67c1fd89d54
seon207 <83705020+seon207@users.noreply.github.com>
2025-01-02 15:13:09 +0900

    [Silver III] Title: 크면서 작은 수, Time: 116 ms, Memory: 14932 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.java
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.java
@@ -4,57 +4,57 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-	static int temp[], result[];
-	static boolean visited[], flag;
-	static String str, answer;
+    static int[] arr, result;
+    static boolean[] visited;
+    static boolean flag;
+    static String ans, s;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		str = bf.readLine();
+        s = br.readLine();
 
-		temp = new int[str.length()];
-		result = new int[str.length()];
-		visited = new boolean[str.length()];
-		flag = false;
+        arr = new int[s.length()];
+        result = new int[s.length()];
+        visited = new boolean[s.length()];
+        flag = false;
 
-		for (int i = 0; i < str.length(); i++) {
-			temp[i] = str.charAt(i) - '0';
-		}
+        for (int i = 0; i < s.length(); i++) {  // 숫자로 변환해서 저장
+            arr[i] = s.charAt(i) - '0';
+        }
 
-		Arrays.sort(temp);
+        Arrays.sort(arr);   // 정렬
 
-		search(0);
+        search(0);  // 순열
 
-		if (!flag)
-			System.out.println(0);
-	}
+        if (!flag)  // 못찾은 경우
+            System.out.println(0);
+    }
 
-	private static void search(int idx) {
-		if (idx == result.length) {
-			answer="";
-			for (int i = 0; i < result.length; i++) {
-				answer += Integer.toString(result[i]);
-			}
+    private static void search(int idx) {
+        if (idx == result.length) { // 순열 완성
+            ans = "";
+            for (int i = 0; i < result.length; i++) {
+                ans += Integer.toString(result[i]);
+            }
 
-			if (!flag && Integer.parseInt(answer) > Integer.parseInt(str)) {
-				flag = true;
-				System.out.println(answer);
-			}
-
-			return;
-		}
-
-		for (int i = 0; i < temp.length; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				result[idx] = temp[i];
-				search(idx + 1);
-				visited[i] = false;
-				if (flag)
-					return;
-			}
-		}
-	}
+            if (!flag && Integer.parseInt(ans) > Integer.parseInt(s)) { // 더 큰 수를 찾았을 때
+                flag = true;
+                System.out.println(ans);
+            }
+            return;
+        }
+        // 순열 생성
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[idx] = arr[i];
+                search(idx + 1);
+                visited[i] = false;
+                if (flag)
+                    return;
+            }
+        }
+    }
 }
  

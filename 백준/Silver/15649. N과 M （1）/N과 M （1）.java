8fe90d01a5291e388407117bb2505585d7a27153
seon207 <83705020+seon207@users.noreply.github.com>
2025-03-28 02:36:25 +0900

    [Silver III] Title: N과 M (1), Time: 1528 ms, Memory: 52124 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,41 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static boolean[] visited;
-	static int[] numbers;
+    static int N, M;
+    static boolean[] num;   // 가능 여부 확인
+    static int[] list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N + 1];
-		numbers = new int[M];
-		perm(0);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-	public static void perm(int cnt) {
-		if (cnt == M) { // 다 골랐을 때
-			for (int num : numbers) {
-				System.out.print(num + " ");
-			}
-			System.out.println();
-		} else {
+        num = new boolean[N + 1];
+        list = new int[M];
+        perm(0, 0);
+    }
 
-			for (int i = 1; i < N + 1; i++) {
-				if (!visited[i]) {
-					numbers[cnt] = i;
-					visited[i] = true;
-					perm(cnt + 1);
-					visited[i] = false;
-				}
-			}
-		}
-	}
+    public static void perm(int depth, int idx) {
+        if (depth == M) {
+            for (int num : list) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!num[i]) {
+                num[i] = true;
+                list[idx] = i;
+                perm(depth + 1, idx + 1);
+                num[i] = false;
+            }
+        }
+
+    }
 }

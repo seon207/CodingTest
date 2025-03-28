5ea724cab1a105bde80fc4db90ebb4180ebfc09e
seon207 <83705020+seon207@users.noreply.github.com>
2025-03-29 04:55:06 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -5,44 +5,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static StringBuilder sb;
-	static int N, M;
-	static int[] arr;
-	static int[] result;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		sb = new StringBuilder();
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		result = new int[M];
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i=0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		perm(0, 0);
-		System.out.println(sb);
-	}
-	
-	public static void perm(int idx, int start) {
-		if (idx >= M) {
-			for (int i = 0 ; i<M; i++) {
-				sb.append(result[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = start; i<N; i++) {
-			result[idx] = arr[i];
-			perm(idx+1, i+1);
-		}
-	}
+    static int N, M;
+    static int[] num;
+    static int[] list;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        list = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        perm(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void perm(int depth, int start) {
+        if (depth == M) {
+            for (int num : list) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            list[depth] = num[i];
+            perm(depth + 1, i + 1);
+        }
+
+    }
 }

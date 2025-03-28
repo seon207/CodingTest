1d91c442954206c1154cfa29ef5dccfe49eed2c4
seon207 <83705020+seon207@users.noreply.github.com>
2025-03-29 04:38:42 +0900

    [Silver III] Title: N과 M (5), Time: 300 ms, Memory: 29756 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,50 +1,56 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static int n, m;
-    static boolean[] visited;
-    static int[] ans;
-    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] num;
+    static int[] list;
+    static StringBuilder sb;
+    static boolean[] is_possible;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
 
-        arr = new int[n];
-        ans = new int[n];
-        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
+        num = new int[N];
+        list = new int[M];
+        is_possible = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
         }
-        
-        
-        Arrays.sort(arr);
-        visited = new boolean[n];
-        dfs(0);
-        System.out.print(sb.toString());
+
+        Arrays.sort(num);
+        comb(0);
+
+        System.out.println(sb);
     }
 
-    public static void dfs(int depth) {
-        if (depth == m){
-            for (int i = 0; i < m; i++){
-                sb.append(ans[i] + " ");
+    public static void comb(int depth) {
+        if (depth == M) {
+            for (int num : list) {
+                sb.append(num).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for (int i = 0; i < n; i++){
-            if (!visited[i]){
-                visited[i] = true;
-                ans[depth] = arr[i];
-                dfs(depth + 1);
-                visited[i] = false;
+
+        for (int i = 0; i < N; i++) {
+            if (!is_possible[i]) {
+                is_possible[i] = true;
+                list[depth] = num[i];
+                comb(depth + 1);
+                is_possible[i] = false;
             }
         }
     }

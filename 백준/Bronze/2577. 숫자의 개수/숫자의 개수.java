45e38ad27dcf00d551c6295402b05a52a58f2c58
seon207 <83705020+seon207@users.noreply.github.com>
2025-07-10 18:24:37 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,22 +1,20 @@
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
         int[] nums = new int[10];
+        int result = A * B * C;
 
-        int result = a * b * c;
-        String value = String.valueOf(result);
-
-        for (int i = 0; i < value.length(); i++) {
-            nums[value.charAt(i) - 48]++;
+        String r = String.valueOf(result);
+        for (int i = 0; i < r.length(); i++) {
+            int num = Integer.parseInt(String.valueOf(r.charAt(i)));
+            nums[num]++;
         }
 
         for (int i : nums) {

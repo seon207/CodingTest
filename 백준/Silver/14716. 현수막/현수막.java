eac15880691e0fa6e335d7d716040547a2b670be
seon207 <83705020+seon207@users.noreply.github.com>
2025-08-07 14:53:42 +0900

    [Silver I] Title: 현수막, Time: 204 ms, Memory: 25472 KB -BaekjoonHub

--- 백준/Silver/14716. 현수막/현수막.java
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -63,15 +63,77 @@
 //    }
 //}
 
-import java.io.*;
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//    static int N, M, count;
+//    static int[][] map;
+//    static boolean[][] visited;
+//
+//    // 상하좌우 대각선
+//    static int[] dx = {0, 1, 0, -1, 1, -1, -1, 1};
+//    static int[] dy = {1, 0, -1, 0, 1, -1, 1, -1};
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        N = Integer.parseInt(st.nextToken());
+//        M = Integer.parseInt(st.nextToken());
+//
+//        map = new int[N][M];
+//        visited = new boolean[N][M];
+//        for (int i = 0; i < N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            for (int j = 0; j < M; j++) {
+//                map[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < M; j++) {
+//                // 방문 안했고 1인 경우에만 시작
+//                if (!visited[i][j] && map[i][j] == 1) {
+//                    bfs(i, j);
+//                    count += 1;
+//                }
+//            }
+//        }
+//
+//        System.out.println(count);
+//
+//    }
+//
+//    public static void bfs(int x, int y) {
+//        Queue<int[]> queue = new LinkedList<>();
+//        queue.add(new int[]{x, y});
+//        visited[x][y] = true;
+//
+//        while (!queue.isEmpty()) {
+//            int[] now = queue.poll();
+//            int cx = now[0];
+//            int cy = now[1];
+//
+//            for (int i = 0; i < 8; i++) {
+//                int nx = cx + dx[i];
+//                int ny = cy + dy[i];
+//
+//                if (nx < 0 || ny < 0 || nx > N - 1 || ny > M - 1 || visited[nx][ny] || map[nx][ny] == 0)
+//                    continue;
+//                visited[nx][ny] = true;
+//                queue.offer(new int[]{nx, ny});
+//            }
+//        }
+//    }
+//}
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N, M, count;
-    static int[][] map;
     static boolean[][] visited;
-
-    // 상하좌우 대각선
+    static int[][] map;
     static int[] dx = {0, 1, 0, -1, 1, -1, -1, 1};
     static int[] dy = {1, 0, -1, 0, 1, -1, 1, -1};
 
@@ -83,6 +145,7 @@ public class Main {
 
         map = new int[N][M];
         visited = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -92,36 +155,25 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // 방문 안했고 1인 경우에만 시작
                 if (!visited[i][j] && map[i][j] == 1) {
-                    bfs(i, j);
-                    count += 1;
+                    visited[i][j] = true;
+                    dfs(i, j);
+                    count++;
                 }
             }
         }
 
         System.out.println(count);
-
     }
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        visited[x][y] = true;
+    public static void dfs(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int cx = now[0];
-            int cy = now[1];
-
-            for (int i = 0; i < 8; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if (nx < 0 || ny < 0 || nx > N - 1 || ny > M - 1 || visited[nx][ny] || map[nx][ny] == 0)
-                    continue;
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && map[nx][ny] == 1) {
                 visited[nx][ny] = true;
-                queue.offer(new int[]{nx, ny});
+                dfs(nx, ny);
             }
         }
     }

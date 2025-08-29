7bcc2190a7de2bd02107f716fd6a6490b1e17a12
seon207 <83705020+seon207@users.noreply.github.com>
2025-08-29 17:27:18 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,34 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int T;
-    static int num;
-    static int count;
+    static int T, n;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
-        int[] dp = new int[11];
-
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-
-        for (int i = 4; i < 11; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        }
-
         for (int i = 0; i < T; i++) {
-            num = Integer.parseInt(br.readLine());
-            System.out.println(dp[num]);
+            n = Integer.parseInt(br.readLine());
+            dp = new int[11];
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int j = 4; j < 11; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+
+            System.out.println(dp[n]);
         }
     }
 }
-
-
-

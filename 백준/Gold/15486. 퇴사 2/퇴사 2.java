3e18d53de938088ce11b8a30ca1c5abc880121da
seon207 <83705020+seon207@users.noreply.github.com>
2025-08-30 01:12:34 +0900

    [Gold V] Title: 퇴사 2, Time: 676 ms, Memory: 316108 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] T, P, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        T = new int[N + 1];
+        P = new int[N + 1];
+        dp = new int[N + 1];
+
+        // 입력
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            if (i + T[i] <= N) {
+                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+            }
+            dp[i + 1] = Math.max(dp[i], dp[i + 1]);
+        }
+
+        System.out.println(dp[N]);
+    }
+}

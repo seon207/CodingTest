801e5dc6b3c1cb8432672d27cdaaf1d806fd9d46
seon207 <83705020+seon207@users.noreply.github.com>
2025-08-29 17:19:00 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int T;
+    static int num;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < T; i++) {
+            num = Integer.parseInt(br.readLine());
+            System.out.println(dp[num]);
+        }
+    }
+}
+
+
+

e4d016d2005944155d7d2342535b8a7d6b5a178d
seon207 <83705020+seon207@users.noreply.github.com>
2025-10-23 15:32:45 +0900

    [Silver II] Title: It’s tough being a teen!, Time: 116 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Silver/6858. It’s tough being a teen！/It’s tough being a teen！.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] indegrees = new int[8];
+    static List<List<Integer>> list;
+    static boolean flag;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        list = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            list.add(new ArrayList<>());
+        }
+        flag = true;
+        // 초기 상태
+        add(1, 7);
+        add(1, 4);
+        add(2, 1);
+        add(3, 4);
+        add(3, 5);
+        while (true) {
+            int first = Integer.parseInt(br.readLine());
+            int second = Integer.parseInt(br.readLine());
+
+            if (first == 0 && second == 0) {
+                break;
+            }
+
+            add(first, second);
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i <= 7; i++) {
+            if (indegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            result.add(cur);
+
+            for (int next : list.get(cur)) {
+                indegrees[next]--;
+                if (indegrees[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        if (result.size() < 7) {
+            System.out.println("Cannot complete these tasks. Going to bed.");
+        } else {
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void add(int a, int b) {
+        list.get(a).add(b);
+        indegrees[b]++;
+    }
+}

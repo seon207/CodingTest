74330de21a559843f1f95345d0336be6d637954c
seon207 <83705020+seon207@users.noreply.github.com>
2025-11-06 15:18:15 +0900

    [level 3] Title: 숫자 게임, Time: 59.84 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int j = A.length-1;
+        for(int i = A.length-1; i >= 0; i--){
+            if(A[i] < B[j]){
+               answer++; 
+                j--;
+            }
+        }
+        
+        return answer;
+    }
+    
+}

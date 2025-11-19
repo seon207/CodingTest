6748119894f9f83ec9e46a3141077d61ccada18b
seon207 <83705020+seon207@users.noreply.github.com>
2025-11-19 16:44:59 +0900

    [Silver III] Title: 수열, Time: 240 ms, Memory: 24636 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,36 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int max;
-    static int N,K;
-    static int[] temp;
+    static int N, K, max;
+    static int[] arr, sums;
+
     public static void main(String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); 
+        N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        temp = new int[N]; 
-        max = 0;
-        
+
+        arr = new int[N + 1];
+        sums = new int[N + 1];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            temp[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sums[i] = sums[i - 1] + arr[i];
         }
 
-        for (int i = 0; i < K; i++) {
-            max = max + temp[i]; // 0 ~ K까지의 값 모두 더해서 max로 설정
+        max = Integer.MIN_VALUE;
+        for (int i = K; i <= N; i++) {
+            int val = sums[i] - sums[i - K];
+            max = Math.max(val, max);
         }
-        
-        int sum = max;
-        for (int i = K; i < N; i++) {
-            // 새로운 값 더하기, 이전 값 빼기
-            sum = sum - temp[i - K] + temp[i];
-            max = Math.max(max, sum); // 수 갱신
-        }
+
 
         System.out.println(max);
     }

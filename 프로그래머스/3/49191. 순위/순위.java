3984f2dd7a5b5302ad2b564a069142f643dafe54
seon207 <83705020+seon207@users.noreply.github.com>
2025-11-27 15:41:07 +0900

    [level 3] Title: 순위, Time: 17.87 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -15,17 +15,12 @@ class Solution {
             graph[lose][win] = -1;
         }
         
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n; j++){
-                for(int k = 1; k<=n; k++){
+        for(int k = 1; k<=n; k++){
+            for(int i = 1; i<=n; i++){
+                for(int j = 1; j<=n; j++){
                     if(graph[i][k] == 1 && graph[k][j] == 1){
                         graph[i][j] = 1;
                         graph[j][i] = -1;
-                    }
-                    
-                    if(graph[i][k] == -1 && graph[k][j] == -1){
-                        graph[i][j] = -1;
-                        graph[j][i] = 1;
                     }
                 }
             }

bfaf912bd37082cfb751d0118f2e34700cdbed8a
seon207 <83705020+seon207@users.noreply.github.com>
2025-11-28 01:53:39 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 528 ms, Memory: 62716 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -1,39 +1,44 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
+    static int N, M;
+    static int[] ground, dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());   // 연병장 크기
-        int M = Integer.parseInt(st.nextToken());   // 조교의 수
-
+        ground = new int[N];
+        dp = new int[N];
         st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N + 1];
-        int[] sums = new int[N + 2];
-
-        for (int i = 1; i < N + 1; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            ground[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < M; i++) {    //커멘드
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int dirt = Integer.parseInt(st.nextToken());
 
-            // 처음과 끝만 계산해서 누적합으로 처리
-            sums[a] += k;
-            sums[b + 1] -= k;
+            dp[start - 1] += dirt;
+            if (end < N) {
+                dp[end] += -dirt;
+            }
         }
 
-        for (int i = 1; i < N + 1; i++) {   // 누적합 계산
-            sums[i] = sums[i - 1] + sums[i];
+        int add = 0;
+        for (int i = 0; i < N; i++) {
+            add += dp[i];
+            sb.append(add + ground[i]).append(" ");
         }
 
-        for (int i = 1; i < N + 1; i++) {
-            System.out.print(arr[i] + sums[i] + " ");
-        }
+        System.out.println(sb);
+
     }
 }

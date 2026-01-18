0148eb2b275073ded6c9170cd992dadf76c4fede
seon207 <83705020+seon207@users.noreply.github.com>
2026-01-19 00:10:10 +0900

    [Silver III] Title: N과 M (1), Time: 448 ms, Memory: 30100 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,12 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, M;
-    static boolean[] num;   // 가능 여부 확인
-    static int[] list;
+    static boolean[] visited;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,28 +12,28 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        num = new boolean[N + 1];
-        list = new int[M];
-        perm(0, 0);
+        visited = new boolean[N + 1];
+        arr = new int[M];
+
+        perm(0);
     }
 
-    public static void perm(int depth, int idx) {
-        if (depth == M) {
-            for (int num : list) {
-                System.out.print(num + " ");
+    public static void perm(int depth) {
+        if (depth == M) { // 전부 선택함
+            StringBuilder sb = new StringBuilder();
+            for (int a : arr) {
+                sb.append(a).append(" ");
             }
-            System.out.println();
+            System.out.println(sb);
             return;
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!num[i]) {
-                num[i] = true;
-                list[idx] = i;
-                perm(depth + 1, idx + 1);
-                num[i] = false;
-            }
+            if (visited[i]) continue;
+            arr[depth] = i;
+            visited[i] = true;
+            perm(depth + 1);
+            visited[i] = false;
         }
-
     }
 }

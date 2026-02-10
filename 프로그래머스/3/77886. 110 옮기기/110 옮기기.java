51ed09a32fc6e9f7ff3ff85d4f2a7ec5f5c77c95
seon207 <83705020+seon207@users.noreply.github.com>
2026-02-11 01:57:46 +0900

    [level 3] Title: 110 옮기기, Time: 75.77 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -4,46 +4,40 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] s) {
         String[] answer = new String[s.length];
+        int loc = 0;
         for(int i = 0; i < s.length; i++){
-            String result = exchange(s[i]);
-            answer[i] = result;
-        }
-        return answer;
-    }
-    
-    public static String exchange(String s){
-        int size = s.length();
-        char[] arr = new char[size];
-        int top = 0;
-        int count = 0;
-        for(int i = 0; i < size; i++){
-            char c = s.charAt(i);
-            arr[top++] = c;
+            int count = 0;
+            int idx = 0;
+            String str = s[i];
+            char[] arr = new char[str.length()];
+            for(int j = 0; j < str.length(); j++){
+                arr[idx++] = str.charAt(j);
+                if(idx >= 3){   // 110 모임
+                    if(arr[idx-3] == '1' && arr[idx-2] == '1' && arr[idx-1] == '0'){
+                        count++;
+                        idx-=3;
+                    }
+                }
+            }
             
-            if(top >= 3){
-            if(arr[top-3] == '1' && arr[top-2] == '1' && arr[top-1] == '0'){
-                top -= 3;
-                count++;
+            StringBuilder sb = new StringBuilder();
+            String nstr = new String(arr, 0, idx);  // 처음부터 idx 위치까지 새로운 문자열
+            int last = nstr.lastIndexOf('0');    // 마지막 0 위치
+            if(last == -1){  // 맨앞
+                for(int k = 0 ; k < count; k++){
+                    sb.append("110");
+                }
+                sb.append(nstr);
+            }else{
+                sb.append(nstr, 0, last + 1);
+                for(int k = 0; k < count; k++){
+                    sb.append("110");
+                }
+                sb.append(nstr.substring(last + 1));
             }
-            }
+            answer[loc++] = sb.toString();
         }
         
-        String left = new String(arr, 0, top);
-        
-        int idx = left.lastIndexOf('0');
-        StringBuilder sb = new StringBuilder();
-        if(idx == -1){  // 0 없음 -> 맨앞
-            for(int i = 0; i < count; i++){
-                sb.append("110");
-            }
-            sb.append(left);
-        }else{
-            sb.append(left, 0, idx + 1);
-            for(int i = 0; i<count; i++){
-                sb.append("110");
-            }
-            sb.append(left.substring(idx + 1));
-        }
-        return sb.toString();
+        return answer;
     }
 }

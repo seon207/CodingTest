228a004ba66aa5f195f042ea5ca2f73e8733aec7
seon207 <83705020+seon207@users.noreply.github.com>
2026-04-07 21:32:36 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,1 @@
+select a.product_id, a.product_name, b.amount * a.price as total_sales from food_product a join (select product_id, sum(amount) amount from food_order where produce_date like '2022-05%' group by product_id) b on a.product_id = b.product_id order by total_sales desc, product_id;

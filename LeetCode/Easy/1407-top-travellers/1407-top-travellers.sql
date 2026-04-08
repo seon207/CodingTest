38a51908c8d1034c44ecb46f6f8dd308dc046953
seon207 <83705020+seon207@users.noreply.github.com>
2026-04-09 00:12:48 +0900

    Time: 768 ms (78.44%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Easy/1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,1 @@
+select name, ifnull(b.distance,0) as travelled_distance from users a left join (select user_id, sum(distance) distance from rides group by user_id) b on a.id = b.user_id order by distance desc, name asc;

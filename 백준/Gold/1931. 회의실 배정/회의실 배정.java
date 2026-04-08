af7022b1b7c82dbfb135bbcb80fd2dfe07985282
seon207 <83705020+seon207@users.noreply.github.com>
2026-04-08 23:59:36 +0900

    [Gold V] Title: 회의실 배정, Time: 500 ms, Memory: 44392 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,50 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static class Edge implements Comparable<Edge> {
+        int s;
+        int e;
+
+        public Edge(int s, int e) {
+            this.s = s;
+            this.e = e;
+
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.e != o.e ? this.e - o.e : this.s - o.s;
+        }
+    }
+
     static int N;
-    static ArrayList<conference> list;
-    static conference[] conferences;
+    static ArrayList<Edge> list;
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         list = new ArrayList<>();
-        conferences = new conference[N];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
 
-            conferences[i] = new conference(start, end);
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            Edge edge = new Edge(s, e);
+            list.add(edge);
         }
 
-        // 정렬
-        Arrays.sort(conferences);
-        list.add(conferences[0]);
-        int last = conferences[0].e;    // 끝나는 시간
+        Collections.sort(list);
 
+        Edge front = list.get(0);   // 제일 처음
+        count = 1;
         for (int i = 1; i < N; i++) {
-            if (conferences[i].s >= last) { // 이후에 시작
-                list.add(conferences[i]);
-                last = conferences[i].e;
+            if (front.e <= list.get(i).s) {
+                front = list.get(i);
+                count++;
             }
         }
 
-        System.out.println(list.size());
-    }
-
-    public static class conference implements Comparable<conference> {
-        int s;
-        int e;
-
-        public conference(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
-
-        @Override
-        public int compareTo(conference o) {
-            return this.e != o.e ? this.e - o.e : this.s - o.s;
-        }
+        System.out.println(count);
     }
 }
